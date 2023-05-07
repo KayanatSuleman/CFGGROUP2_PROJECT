0e3904f7758988d2bc91c31a9f1e03e38502d6f9
Kayanat Suleman <kayanatsuleman@Kayanats-MacBook-Pro.local>
2023-05-07 16:53:24 +0100

    insert commit message here

---
+++ src/Kayanat.js
@@ -0,0 +1,15 @@
+function Kayanat () {
+    return (
+        <div>
+            <h1>Kayanat</h1>
+            <br></br>
+            <p> 
+                <li>My favourite hobby is ice skating</li>
+                <li>I enjoy dancing as it is a great way to exercise and enjoy music!</li>
+
+            </p>
+        </div>
+    )
+};
+
+export default Kayanat;
