31225fbf89aad4eb841c150069f8cff0d207f3eb
Siham Dahir <98513419+sihamad@users.noreply.github.com>
2023-05-07 16:57:26 +0100

    Adding Siham's content

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Karen from  './Karen.js';
 import Arianne from './Arianne.js';
+import Siham from './Siham';
 
 function App() {
   return (
     <div className="App">
         <Karen />
         <Arianne />
+        <Siham />
     </div>
   );
 }
