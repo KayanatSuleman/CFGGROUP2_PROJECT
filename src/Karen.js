218cb9166c735ec5fedee2e5483eb86b5088d0ae
Karen <karenyaa@live.co.uk>
2023-05-07 16:08:04 +0100

    app for project created and component created for Karen's hobby

---
+++ src/Karen.js
@@ -0,0 +1,13 @@
+function Karen () {
+    return (
+        <div>
+            <h1>Karen</h1>
+            <br></br>
+            <p> 
+                <li>My favourite hobby is dancing!</li>
+                <li>I enjoy dancing as it is a great way to exercise and enjoy music!</li>
+
+            </p>
+        </div>
+    )
+};
