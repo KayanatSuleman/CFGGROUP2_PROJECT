4a25cd6ed7aaf1da4955f90ac28714cf11b2a9ba
Kayanat Suleman <kayanatsuleman@Kayanats-MacBook-Pro.local>
2023-05-07 16:57:32 +0100

    app.js updated file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Karen from  './Karen.js';
 import Arianne from './Arianne.js';
+import Kayanat from './Kayanat.js';
 
 function App() {
   return (
     <div className="App">
         <Karen />
         <Arianne />
+        <Kayanat />
     </div>
   );
 }
